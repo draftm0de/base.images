20f9047733c8524c1ea1808c21ab8310c4eeeb83
Peter Fux <peter.fux@draftmode.io>
2024-10-15 22:38:32 +0200

    set SAH_protection fix

--- caddy/script.sh
+++ caddy/script.sh
@@ -14,23 +14,24 @@ SCRIPT_PATH="/home/admin/projects/draftm0de/projects/kernel.deployment"
 
 echo "[Notice] Build image..."
 BUILT_IMAGE_NAME="$REPOSITORY:latest"
-("${SCRIPT_PATH}/docker.sh" "build" "--build-args=.build_args" "-t $BUILT_IMAGE_NAME" ".")
+# ("${SCRIPT_PATH}/docker.sh" "build" "--build-args=.build_args" "-t $BUILT_IMAGE_NAME" ".")
 echo "[Notice] > build successfully"
 
 # get local SHA from built image
 echo "[Notice] Protect built SHA against remote"
-SHA=$("${SCRIPT_PATH}/docker.sh" "image sha" "$BUILT_IMAGE_NAME")
+# SHA=$("${SCRIPT_PATH}/docker.sh" "image sha" "$BUILT_IMAGE_NAME")
 echo "[Notice] Local SHA:$SHA"
-("${SCRIPT_PATH}/docker.sh" "image tags" "$REPOSITORY" "--remote" "--sha=$SHA" "--exists")
+# ("${SCRIPT_PATH}/docker.sh" "image tags" "$REPOSITORY" "--remote" "--sha=$SHA" "--exists")
 echo "[Notice] > Protection successfully"
 
 # get local SHA from built image
 echo "[Notice] Get latest remote tag for $BUILT_IMAGE_NAME "
-LATEST_TAG=$("${SCRIPT_PATH}/docker.sh" "image tags" "$BUILT_IMAGE_NAME" "--remote" "--latest=patch")
-#LATEST_TAG="1.2.1"
+# LATEST_TAG=$("${SCRIPT_PATH}/docker.sh" "image tags" "$BUILT_IMAGE_NAME" "--remote" "--latest=patch")
 echo "[Notice] > latest remote tag for $BUILT_IMAGE_NAME: $LATEST_TAG"
 
 echo "[Notice] Tag docker image"
+LATEST_TAG=""
+# TAGS=$("${SCRIPT_PATH}/docker.sh" "image tag" "$BUILT_IMAGE_NAME" "$REPOSITORY:${LATEST_TAG:-0.0.0}" "--tag-increase=1.2" "--tag-level=3")
 TAGS=$("${SCRIPT_PATH}/docker.sh" "image tag" "$BUILT_IMAGE_NAME" "$REPOSITORY:$LATEST_TAG" "--tag-increase" "--tag-level=3")
 echo "[Notice] > docker image tagged successfully"
 
